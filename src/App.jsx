829d70fbb109c5b030f21594e50bfe4c25fd81ab
Oliver Twist <kipkiruigideon890@gmail.com>
2026-06-12 14:36:05 +0300

    fix: add missing Navigate import, add crossorigin to font link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { LanguageProvider } from './utils/lang';
 import { CartProvider } from './context/CartContext';
 import { NiaChatProvider } from './context/NiaChatContext';
